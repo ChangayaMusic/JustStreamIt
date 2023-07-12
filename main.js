8d7961adb7c6cc07513275ac6ea3177eeee53758
ChangayaMusic <121641916+ChangayaMusic@users.noreply.github.com>
2023-07-12 16:17:36 +0200

    working on carousel

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
   const videoTitleElement = document.getElementById('video-title');
   const videoImageElement = document.getElementById('video-image');
-  const topRatedMoviesContainer = document.getElementById('top-rated-movies');
   const carouselContainer = document.getElementById('carousel-container');
-  const prevButton = document.getElementById('prev-button');
-  const nextButton = document.getElementById('next-button');
-  let slidePosition = 0;
-  const moviesPerPage = 8;
-  const actionCarouselContainer = document.getElementById('action-carousel-container'); 
+  const actionCarouselContainer = document.getElementById('action-carousel-container');
+  const prevButtonRanked = document.getElementById('prev-button-ranked');
+  const nextButtonRanked = document.getElementById('next-button-ranked');
+  const prevButtonAction = document.getElementById('prev-button-action');
+  const nextButtonAction = document.getElementById('next-button-action');
+  let slidePositionRanked = 0;
+  let slidePositionAction = 0;
 
   function fetchHighestRatedMovie() {
     fetch('http://localhost:8000/api/v1/titles/?sort_by=-imdb_score')
@@ -25,17 +26,13 @@ document.addEventListener('DOMContentLoaded', function() {
       });
   }
 
-  function fetchAllPages(url) {
+  function fetchMovies(url, container) {
     fetch(url)
       .then(response => response.json())
       .then(data => {
         const movies = data.results;
-        const nextUrl = data.next;
 
-        // Exclure le film le mieux noté
-        const filteredMovies = movies.slice(1);
-
-        filteredMovies.forEach(movie => {
+        movies.forEach(movie => {
           const imageUrl = movie.image_url;
           const altText = movie.title;
 
@@ -43,86 +40,63 @@ document.addEventListener('DOMContentLoaded', function() {
           imageElement.src = imageUrl;
           imageElement.alt = altText;
 
-          carouselContainer.appendChild(imageElement);
+          container.appendChild(imageElement);
         });
 
-        if (nextUrl) {
-          fetchAllPages(nextUrl);
-        } else {
-          // Sinon, initialiser le carrousel
-          initCarousel();
-        }
+        initCarousel(container);
       })
       .catch(error => {
         console.error('Error:', error);
       });
   }
-  function fetchActionMovies(url) { 
-    fetch(url)
-      .then(response => response.json())
-      .then(data => {
-        const movies = data.results;
-        const nextUrl = data.next;
-  
-        
-        const filteredMovies = movies
-  
-        filteredMovies.forEach(movie => {
-          const imageUrl = movie.image_url;
-          const altText = movie.title;
-  
-          const imageElement = document.createElement('img');
-          imageElement.src = imageUrl;
-          imageElement.alt = altText;
-  
-          actionCarouselContainer.appendChild(imageElement); 
-        });
-  
-        if (nextUrl) {
-          fetchActionMovies(nextUrl);
-        } else {
-        
-          initCarousel();
-        }
-      })
-      .catch(error => {
-        console.error('Error:', error);
-      });
-  }
-  
-  function initCarousel() {
-    function updateSlidePosition() {
-      carouselContainer.style.transform = `translateX(${slidePosition}px)`;
-    }
+
+  function initCarousel(container) {
+    const prevButton = container.previousElementSibling;
+    const nextButton = container.nextElementSibling;
+    const imageElements = container.getElementsByTagName('img');
 
     prevButton.addEventListener('click', function() {
-      if (slidePosition === 0) {
-        slidePosition =
-          -imageElement.offsetWidth * carouselContainer.children.length + 
-          (carouselContainer.offsetWidth % (carouselContainer.children.length * 25));
-      } else {
-        slidePosition += imageElement.offsetWidth * moviesPerPage; 
-      }
-      updateSlidePosition();
+      slidePositionRanked += container.offsetWidth; // Scroll by the width of the container
+      updateSlidePosition(container, slidePositionRanked);
     });
 
     nextButton.addEventListener('click', function() {
-      if (
-        slidePosition ===
-        -imageElement.offsetWidth * carouselContainer.children.length + 
-          (carouselContainer.offsetWidth % (carouselContainer.children.length * 25))
-      ) {
-        slidePosition = 0;
-      } else {
-        slidePosition -= imageElement.offsetWidth * moviesPerPage; 
-      }
-      updateSlidePosition();
+      slidePositionRanked -= container.offsetWidth; // Scroll by the width of the container
+      updateSlidePosition(container, slidePositionRanked);
     });
+
+    container.style.width = `${imageElements.length * imageElements[0].offsetWidth}px`; // Adjust container width based on the number of images
   }
 
-  fetchAllPages('http://localhost:8000/api/v1/titles/?imdb_score_min=8&sort_by=-imdb_score');
+  function updateSlidePosition(container, slidePosition) {
+    container.style.transform = `translateX(${slidePosition}px)`;
+    container.style.transition = 'transform 0.3s ease-in-out';
+    setTimeout(function() {
+      container.style.transition = '';
+    }, 300);
+  }
+
   fetchHighestRatedMovie();
-  fetchActionMovies('http://localhost:8000/api/v1/titles/?genre=Action&sort_by=-imdb_score'); // Change the URL to match your API endpoint and the function name to match
-  
-  
+  fetchMovies('http://localhost:8000/api/v1/titles/?imdb_score_min=8&sort_by=-imdb_score', carouselContainer);
+  fetchMovies('http://localhost:8000/api/v1/titles/?genre=Action&sort_by=-imdb_score', actionCarouselContainer);
+
+  prevButtonRanked.addEventListener('click', function() {
+    slidePositionRanked += carouselContainer.offsetWidth; // Scroll by the width of the container
+    updateSlidePosition(carouselContainer, slidePositionRanked);
+  });
+
+  nextButtonRanked.addEventListener('click', function() {
+    slidePositionRanked -= carouselContainer.offsetWidth; // Scroll by the width of the container
+    updateSlidePosition(carouselContainer, slidePositionRanked);
+  });
+
+  prevButtonAction.addEventListener('click', function() {
+    slidePositionAction += actionCarouselContainer.offsetWidth; // Scroll by the width of the container
+    updateSlidePosition(actionCarouselContainer, slidePositionAction);
+  });
+
+  nextButtonAction.addEventListener('click', function() {
+    slidePositionAction -= actionCarouselContainer.offsetWidth; // Scroll by the width of the container
+    updateSlidePosition(actionCarouselContainer, slidePositionAction);
+  });
 });
