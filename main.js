ba1e470ebd25e0b2591caf0b56b27262f251b83f
ChangayaMusic <121641916+ChangayaMusic@users.noreply.github.com>
2023-07-17 16:06:51 +0200

    button not working

--- main.js
+++ main.js
@@ -27,42 +27,58 @@ document.addEventListener('DOMContentLoaded', function() {
   }
 
   function fetchMovies(url, container) {
-    fetch(url)
-      .then(response => response.json())
-      .then(data => {
-        const movies = data.results;
+    const allMovies = []; // Store all fetched movies from all pages
 
-        movies.forEach(movie => {
-          const imageUrl = movie.image_url;
-          const altText = movie.title;
+    function fetchPage(url) {
+      fetch(url)
+        .then(response => response.json())
+        .then(data => {
+          const movies = data.results;
 
-          const imageElement = document.createElement('img');
-          imageElement.src = imageUrl;
-          imageElement.alt = altText;
+          movies.forEach(movie => {
+            const imageUrl = movie.image_url;
+            const altText = movie.title;
 
-          container.appendChild(imageElement);
+            const imageElement = document.createElement('img');
+            imageElement.src = imageUrl;
+            imageElement.alt = altText;
+
+            container.appendChild(imageElement);
+            allMovies.push(movie); // Add movie to the allMovies array
+          });
+
+          // Check if there are more pages and fetch them recursively
+          if (data.next) {
+            fetchPage(data.next);
+          } else {
+            // All pages have been fetched, initialize carousel
+            initCarousel(container);
+          }
+        })
+        .catch(error => {
+          console.error('Error:', error);
         });
+    }
 
-        initCarousel(container);
-      })
-      .catch(error => {
-        console.error('Error:', error);
-      });
+    fetchPage(url);
   }
 
   function initCarousel(container) {
     const prevButton = container.previousElementSibling;
     const nextButton = container.nextElementSibling;
     const imageElements = container.getElementsByTagName('img');
+    const slidePosition = container.id === 'carousel-container' ? slidePositionRanked : slidePositionAction;
 
     prevButton.addEventListener('click', function() {
-      slidePositionRanked += container.offsetWidth; // Scroll by the width of the container
-      updateSlidePosition(container, slidePositionRanked);
+      slidePosition += container.offsetWidth; // Scroll by the width of the container
+      updateSlidePosition(container, slidePosition);
+      console.log('Previous button clicked in carousel:', container.id);
     });
 
     nextButton.addEventListener('click', function() {
-      slidePositionRanked -= container.offsetWidth; // Scroll by the width of the container
-      updateSlidePosition(container, slidePositionRanked);
+      slidePosition -= container.offsetWidth; // Scroll by the width of the container
+      updateSlidePosition(container, slidePosition);
+      console.log('Next button clicked in carousel:', container.id);
     });
 
     container.style.width = `${imageElements.length * imageElements[0].offsetWidth}px`; // Adjust container width based on the number of images
@@ -80,23 +96,30 @@ document.addEventListener('DOMContentLoaded', function() {
   fetchMovies('http://localhost:8000/api/v1/titles/?imdb_score_min=8&sort_by=-imdb_score', carouselContainer);
   fetchMovies('http://localhost:8000/api/v1/titles/?genre=Action&sort_by=-imdb_score', actionCarouselContainer);
 
+  initCarousel(carouselContainer);
+  initCarousel(actionCarouselContainer);
+
   prevButtonRanked.addEventListener('click', function() {
     slidePositionRanked += carouselContainer.offsetWidth; // Scroll by the width of the container
     updateSlidePosition(carouselContainer, slidePositionRanked);
+    console.log('Previous button clicked in carousel:', carouselContainer.id);
   });
 
   nextButtonRanked.addEventListener('click', function() {
     slidePositionRanked -= carouselContainer.offsetWidth; // Scroll by the width of the container
     updateSlidePosition(carouselContainer, slidePositionRanked);
+    console.log('Next button clicked in carousel:', carouselContainer.id);
   });
 
   prevButtonAction.addEventListener('click', function() {
     slidePositionAction += actionCarouselContainer.offsetWidth; // Scroll by the width of the container
     updateSlidePosition(actionCarouselContainer, slidePositionAction);
+    console.log('Previous button clicked in carousel:', actionCarouselContainer.id);
   });
 
   nextButtonAction.addEventListener('click', function() {
     slidePositionAction -= actionCarouselContainer.offsetWidth; // Scroll by the width of the container
     updateSlidePosition(actionCarouselContainer, slidePositionAction);
+    console.log('Next button clicked in carousel:', actionCarouselContainer.id);
   });
 });
