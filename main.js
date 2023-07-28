e7b93209661962b35816401a34a7c9e9ccbb68ff
ChangayaMusic <121641916+ChangayaMusic@users.noreply.github.com>
2023-07-28 18:20:26 +0200

    modal work, miss close button and hide modal at first

--- main.js
+++ main.js
@@ -21,7 +21,74 @@ document.addEventListener('DOMContentLoaded', function () {
         console.error('Error:', error);
       });
   }
+  function fetchMovieDetailsByTitle(movieTitle) {
+    const encodedTitle = encodeURIComponent(movieTitle);
+    const url = `http://localhost:8000/api/v1/titles/?title_contains=${encodedTitle}`;
+  
+    return fetch(url)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        if (data && data.results && data.results.length > 0) {
+          // Assuming the first movie in the results is the closest match
+          const movieDetails = data.results[0];
+          return movieDetails;
+        } else {
+          throw new Error('Movie not found');
+        }
+      })
+      .catch(error => {
+        console.error('Error fetching movie details:', error);
+        throw error; // Rethrow the error to be caught in the calling function
+      });
+  }
 
+  function openModal(movie) {
+  const modal = document.getElementById('movieModal');
+  const modalTitle = document.getElementById('modalTitle');
+  const modalImage = document.getElementById('modalImage');
+  const modalDescription = document.getElementById('modalDescription');
+
+  modalTitle.textContent = movie.title;
+  modalImage.src = movie.image_url;
+
+  fetchMovieDetailsByTitle(movie.title)
+    .then(movieDetails => {
+      console.log('API Response:', movieDetails);
+
+      // Check if the required properties exist in the movieDetails object
+      const description = movieDetails.description || 'Description not available';
+      const year = movieDetails.year || 'Year not available';
+      const genres = movieDetails.genre ? movieDetails.genre.join(', ') : 'Genres not available';
+      const votes = movieDetails.votes || 'Votes not available';
+      const imdbScore = movieDetails.imdb_score || 'IMDb Score not available';
+      const directors = movieDetails.directors ? movieDetails.directors.join(', ') : 'Directors not available';
+      const actors = movieDetails.actors ? movieDetails.actors.join(', ') : 'Actors not available';
+
+      modalDescription.innerHTML = `
+        <p>Description: ${description}</p>
+        <p>Year: ${year}</p>
+        <p>Genres: ${genres}</p>
+        <p>Votes: ${votes}</p>
+        <p>IMDb Score: ${imdbScore}</p>
+        <p>Directors: ${directors}</p>
+        <p>Actors: ${actors}</p>
+      `;
+
+      modal.style.display = 'block';
+    })
+    .catch(error => {
+      console.error('Error fetching movie details:', error);
+      modal.style.display = 'block';
+    });
+}
+
+  
+  
   function fetchMovies(url, container) {
     function fetchPage(url) {
       fetch(url)
@@ -47,6 +114,11 @@ document.addEventListener('DOMContentLoaded', function () {
             mediaElement.className = 'media-element';
             mediaElement.appendChild(imageElement);
   
+            // Add the click event listener to open the modal
+            imageElement.addEventListener('click', function () {
+              openModal(movie);
+            });
+  
             container.appendChild(mediaElement);
           });
   
@@ -63,9 +135,6 @@ document.addEventListener('DOMContentLoaded', function () {
     fetchPage(url);
   }
   
-  
-  
-
   fetchHighestRatedMovie();
   fetchMovies('http://localhost:8000/api/v1/titles/?imdb_score_min=8&sort_by=-imdb_score', topRatedCarouselContainer);
   fetchMovies('http://localhost:8000/api/v1/titles/?genre=Action&sort_by=-imdb_score', actionCarouselContainer);
