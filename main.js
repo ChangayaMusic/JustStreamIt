ea7efc6fb4db17019833f2d95cd482d4dd4284b0
ChangayaMusic <121641916+ChangayaMusic@users.noreply.github.com>
2023-07-31 15:36:46 +0200

    modal fort video container ok

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', function () {
   const adventureCarouselContainer = document.getElementById('adventure-carousel-container');
 
   function fetchHighestRatedMovie() {
-    fetch('http://localhost:8000/api/v1/titles/?sort_by=-imdb_score')
+    return fetch('http://localhost:8000/api/v1/titles/?sort_by=-imdb_score')
       .then(response => response.json())
       .then(data => {
         const movie = data.results[0];
@@ -16,15 +16,19 @@ document.addEventListener('DOMContentLoaded', function () {
 
         videoTitleElement.textContent = title;
         videoImageElement.src = imageUrl;
+
+        return movie;
       })
       .catch(error => {
         console.error('Error:', error);
+        throw error;
       });
   }
+
   function fetchMovieDetailsByTitle(movieTitle) {
     const encodedTitle = encodeURIComponent(movieTitle);
     const url = `http://localhost:8000/api/v1/titles/?title_contains=${encodedTitle}`;
-  
+
     return fetch(url)
       .then(response => {
         if (!response.ok) {
@@ -34,7 +38,6 @@ document.addEventListener('DOMContentLoaded', function () {
       })
       .then(data => {
         if (data && data.results && data.results.length > 0) {
-          
           const movieDetails = data.results[0];
           return movieDetails;
         } else {
@@ -43,89 +46,91 @@ document.addEventListener('DOMContentLoaded', function () {
       })
       .catch(error => {
         console.error('Error fetching movie details:', error);
-        throw error; 
+        throw error;
       });
   }
 
   function openModal(movie) {
-  const modal = document.getElementById('movieModal');
-  const modalTitle = document.getElementById('modalTitle');
-  const modalImage = document.getElementById('modalImage');
-  const modalDescription = document.getElementById('modalDescription');
+    const modal = document.getElementById('movieModal');
+    const modalTitle = document.getElementById('modalTitle');
+    const modalImage = document.getElementById('modalImage');
+    const modalDescription = document.getElementById('modalDescription');
 
-  modalTitle.textContent = movie.title;
-  modalImage.src = movie.image_url;
+    modalTitle.textContent = movie.title;
+    modalImage.src = movie.image_url;
 
-  fetchMovieDetailsByTitle(movie.title)
-    .then(movieDetails => {
-      console.log('API Response:', movieDetails);
+    fetchMovieDetailsByTitle(movie.title)
+      .then(movieDetails => {
+        console.log('API Response:', movieDetails);
 
-      const description = movieDetails.description || 'Description not available';
-      const year = movieDetails.year || 'Year not available';
-      const genres = movieDetails.genres ? movieDetails.genres.join(', ') : 'Genres not available';
-      const imdbScore = movieDetails.imdb_score || 'IMDb Score not available';
-      const directors = movieDetails.directors ? movieDetails.directors.join(', ') : 'Directors not available';
-      const actors = movieDetails.actors ? movieDetails.actors.join(', ') : 'Actors not available';
-      const worldwideGrossIncome = movieDetails.worldwide_gross_income || 'Worldwide Gross Income not available';
-      const countries = movieDetails.countries ? movieDetails.countries.join(', ') : 'Countries not available';
-      const duration = movieDetails.duration || 'Duration not available';
-     
-      modalDescription.innerHTML = `
-        <p>Description: ${description}</p>
-        <p>Year: ${year}</p>
-        <p>Genres: ${genres}</p>
-      
-        <p>IMDb Score: ${imdbScore}</p>
-        <p>Directors: ${directors}</p>
-        <p>Actors: ${actors}</p>
-        <p>Worldwide Gross Income: ${worldwideGrossIncome}</p>
-        <p>Countries: ${countries}</p>
-        <p>Duration: ${duration} minutes</p>
-      `;
+        const description = movieDetails.description || 'Description not available';
+        const year = movieDetails.year || 'Year not available';
+        const genres = movieDetails.genres ? movieDetails.genres.join(', ') : 'Genres not available';
+        const imdbScore = movieDetails.imdb_score || 'IMDb Score not available';
+        const directors = movieDetails.directors ? movieDetails.directors.join(', ') : 'Directors not available';
+        const actors = movieDetails.actors ? movieDetails.actors.join(', ') : 'Actors not available';
+        const worldwideGrossIncome = movieDetails.worldwide_gross_income || 'Worldwide Gross Income not available';
+        const countries = movieDetails.countries ? movieDetails.countries.join(', ') : 'Countries not available';
+        const duration = movieDetails.duration || 'Duration not available';
 
-      modal.style.display = 'block';
-    })
-    .catch(error => {
-      console.error('Error fetching movie details:', error);
-      modal.style.display = 'block';
-    });
-}
+        modalDescription.innerHTML = `
+          <p>Description: ${description}</p>
+          <p>Year: ${year}</p>
+          <p>Genres: ${genres}</p>
+          <p>IMDb Score: ${imdbScore}</p>
+          <p>Directors: ${directors}</p>
+          <p>Actors: ${actors}</p>
+          <p>Worldwide Gross Income: ${worldwideGrossIncome}</p>
+          <p>Countries: ${countries}</p>
+          <p>Duration: ${duration} minutes</p>
+        `;
 
-  
-  
+        modal.style.display = 'block';
+      })
+      .catch(error => {
+        console.error('Error fetching movie details:', error);
+        modal.style.display = 'block';
+      });
+  }
+
+  function closeModal() {
+    const modal = document.getElementById('movieModal');
+    modal.style.display = 'none';
+  }
+
   function fetchMovies(url, container) {
     function fetchPage(url) {
       fetch(url)
         .then(response => response.json())
         .then(data => {
           const movies = data.results;
-  
+
           movies.forEach(movie => {
             const imageUrl = movie.image_url;
             const altText = movie.title;
-  
+
             const imageElement = document.createElement('img');
             imageElement.src = imageUrl;
             imageElement.alt = altText;
-  
+
             // Add the onerror event handler to handle image loading errors
             imageElement.onerror = function () {
               // Remove the image element from the DOM
               imageElement.remove();
             };
-  
+
             const mediaElement = document.createElement('div');
             mediaElement.className = 'media-element';
             mediaElement.appendChild(imageElement);
-  
+
             // Add the click event listener to open the modal
             imageElement.addEventListener('click', function () {
               openModal(movie);
             });
-  
+
             container.appendChild(mediaElement);
           });
-  
+
           // Check if there are more pages and fetch them recursively
           if (data.next) {
             fetchPage(data.next);
@@ -135,67 +140,77 @@ document.addEventListener('DOMContentLoaded', function () {
           console.error('Error:', error);
         });
     }
-  
+
     fetchPage(url);
   }
-  
-  fetchHighestRatedMovie();
-  fetchMovies('http://localhost:8000/api/v1/titles/?imdb_score_min=8&sort_by=-imdb_score', topRatedCarouselContainer);
-  fetchMovies('http://localhost:8000/api/v1/titles/?genre=Action&sort_by=-imdb_score', actionCarouselContainer);
-  fetchMovies('http://localhost:8000/api/v1/titles/?year=&min_year=&max_year=&imdb_score=&imdb_score_min=&imdb_score_max=&title=&title_contains=&genre=horror&genre_contains=&sort_by=&director=&director_contains=&writer=&writer_contains=&actor=&actor_contains=&country=&country_contains=&lang=&lang_contains=&company=&company_contains=&rating=&rating_contains=', horrorCarouselContainer);
-  fetchMovies('http://localhost:8000/api/v1/titles/?year=&min_year=&max_year=&imdb_score=&imdb_score_min=&imdb_score_max=&title=&title_contains=&genre=adventure&genre_contains=&sort_by=&director=&director_contains=&writer=&writer_contains=&actor=&actor_contains=&country=&country_contains=&lang=&lang_contains=&company=&company_contains=&rating=&rating_contains=', adventureCarouselContainer);
-});
-// Helper function to move the carousel left (previous)
-function moveCarouselPrev(carouselContainer) {
-  const carousel = carouselContainer.querySelector('.carousel-container');
-  const scrollAmount = carousel.scrollWidth / 2;
-  carousel.scrollLeft -= scrollAmount;
-}
 
-// Helper function to move the carousel right (next)
-function moveCarouselNext(carouselContainer) {
-  const carousel = carouselContainer.querySelector('.carousel-container');
-  const scrollAmount = carousel.scrollWidth / 2;
-  carousel.scrollLeft += scrollAmount;
-}
-function closeModal() {
-  const modal = document.getElementById('movieModal');
-  modal.style.display = 'none';
-}
-// Add event listeners for the "Prev" and "Next" buttons
-document.querySelector('.top-rated-carousel .glider-prev').addEventListener('click', function () {
-  moveCarouselPrev(document.querySelector('.top-rated-carousel'));
-});
+  function initializeCarousels() {
+    function moveCarouselPrev(carouselContainer) {
+      const carousel = carouselContainer.querySelector('.carousel-container');
+      const scrollAmount = carousel.scrollWidth / 2;
+      carousel.scrollLeft -= scrollAmount;
+    }
 
-document.querySelector('.top-rated-carousel .glider-next').addEventListener('click', function () {
-  moveCarouselNext(document.querySelector('.top-rated-carousel'));
-});
+    function moveCarouselNext(carouselContainer) {
+      const carousel = carouselContainer.querySelector('.carousel-container');
+      const scrollAmount = carousel.scrollWidth / 2;
+      carousel.scrollLeft += scrollAmount;
+    }
 
-document.querySelector('.action-carousel .glider-prev').addEventListener('click', function () {
-  moveCarouselPrev(document.querySelector('.action-carousel'));
-});
+    // Add event listeners for the "Prev" and "Next" buttons
+    document.querySelector('.top-rated-carousel .glider-prev').addEventListener('click', function () {
+      moveCarouselPrev(document.querySelector('.top-rated-carousel'));
+    });
 
-document.querySelector('.action-carousel .glider-next').addEventListener('click', function () {
-  moveCarouselNext(document.querySelector('.action-carousel'));
-});
+    document.querySelector('.top-rated-carousel .glider-next').addEventListener('click', function () {
+      moveCarouselNext(document.querySelector('.top-rated-carousel'));
+    });
 
-document.querySelector('.horror-carousel .glider-prev').addEventListener('click', function () {
-  moveCarouselPrev(document.querySelector('.horror-carousel'));
-});
+    document.querySelector('.action-carousel .glider-prev').addEventListener('click', function () {
+      moveCarouselPrev(document.querySelector('.action-carousel'));
+    });
 
-document.querySelector('.horror-carousel .glider-next').addEventListener('click', function () {
-  moveCarouselNext(document.querySelector('.horror-carousel'));
-});
+    document.querySelector('.action-carousel .glider-next').addEventListener('click', function () {
+      moveCarouselNext(document.querySelector('.action-carousel'));
+    });
 
-document.querySelector('.adventure-carousel .glider-prev').addEventListener('click', function () {
-  moveCarouselPrev(document.querySelector('.adventure-carousel'));
-});
+    document.querySelector('.horror-carousel .glider-prev').addEventListener('click', function () {
+      moveCarouselPrev(document.querySelector('.horror-carousel'));
+    });
 
-document.querySelector('.adventure-carousel .glider-next').addEventListener('click', function () {
-  moveCarouselNext(document.querySelector('.adventure-carousel'));
-});
-document.getElementById('modalCloseBtn').addEventListener('click', function () {
-  closeModal();
-});
+    document.querySelector('.horror-carousel .glider-next').addEventListener('click', function () {
+      moveCarouselNext(document.querySelector('.horror-carousel'));
+    });
 
+    document.querySelector('.adventure-carousel .glider-prev').addEventListener('click', function () {
+      moveCarouselPrev(document.querySelector('.adventure-carousel'));
+    });
 
+    document.querySelector('.adventure-carousel .glider-next').addEventListener('click', function () {
+      moveCarouselNext(document.querySelector('.adventure-carousel'));
+    });
+
+    document.getElementById('modalCloseBtn').addEventListener('click', function () {
+      closeModal();
+    });
+  }
+
+  fetchHighestRatedMovie()
+    .then(highestRatedMovie => {
+      videoImageElement.addEventListener('click', function () {
+        openModal(highestRatedMovie);
+      });
+
+      // Fetch movie data for all carousels
+      fetchMovies('http://localhost:8000/api/v1/titles/?imdb_score_min=8&sort_by=-imdb_score', topRatedCarouselContainer);
+      fetchMovies('http://localhost:8000/api/v1/titles/?genre=Action&sort_by=-imdb_score', actionCarouselContainer);
+      fetchMovies('http://localhost:8000/api/v1/titles/?genre=horror&sort_by=-imdb_score', horrorCarouselContainer);
+      fetchMovies('http://localhost:8000/api/v1/titles/?genre=adventure&sort_by=-imdb_score', adventureCarouselContainer);
+
+      // Initialize carousel event listeners after loading all the data
+      initializeCarousels();
+    })
+    .catch(error => {
+      console.error('Error fetching highest-rated movie:', error);
+    });
+});
